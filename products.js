43401e79ca9473c557479c1b268151fb1a989532
ZacharyBaldock <93323069+ZacharyBaldock@users.noreply.github.com>
2021-11-14 17:24:07 +0400

    Update products.js

--- products.js
+++ products.js
@@ -6,25 +6,25 @@ let products = [
     "price": 25,
     "image": "https://thumbs.dreamstime.com/b/math-mathematics-word-vintage-letterpress-wood-type-against-number-background-37860245.jpg",
     "availableInventory": 5,
-    "rating": 
+    "rating": 5 
   },
   {
     "id": 1002,
     "title": "Chemistry",
     "description": "For those who have trouble with Chemistry",
     "price": 25,
-    "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F98099-free-chemistry-vector&psig=AOvVaw3SmliSjAKhqzVkKqZzQiMA&ust=1636981141717000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjChZf0l_QCFQAAAAAdAAAAABAD",
+    "image": "https://static.vecteezy.com/system/resources/previews/000/098/099/original/free-chemistry-vector.jpg",
     "availableInventory": 5,
-    "rating": 
+    "rating": 4 
   },
   {
     "id": 1003,
     "title": "Phsisics",
     "description": "For those who have trouble with physics",
     "price": 25,
-    "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fstock-photo%2Fphysics.html&psig=AOvVaw0NWcUjn_DDgclYEtOtOd4x&ust=1636981169596000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCDg6P0l_QCFQAAAAAdAAAAABAL",
+    "image": "https://www.nicepng.com/png/detail/268-2688182_school-project-physics-royalty-free-vector-clip-art.png",
     "availableInventory": 5,
-    "rating": 
+    "rating": 3
   },
   {
     "id": 1004,
@@ -33,7 +33,7 @@ let products = [
     "price": 25,
     "image": "https://moodle.gtni.edu.eg/pluginfile.php/2720/course/overviewfiles/biology-800x393.jpg",
     "availableInventory": 5,
-    "rating": 
+    "rating": 4
   },
   {
     "id": 1005,
@@ -42,16 +42,16 @@ let products = [
     "price": 25,
     "image": "https://st2.depositphotos.com/3591429/5244/i/600/depositphotos_52449961-stock-photo-peoples-arms-holding-word-english.jpg",
     "availableInventory": 5,
-    "rating": 
-  }
-      {
+    "rating": 5
+  },
+  {
     "id": 1006,
     "title": "French",
     "description": "So the subject isn't french to you",
     "price": 25,
     "image": "https://lh3.googleusercontent.com/proxy/9MQGTcKtKv3a6qqeaRdI1UFbfJs0CllWn5XSJALpW9-pMLyMd9Uf7ztapprrzSG877IT8kCgKZ0NNMFS6gSEH7Wa2TC4Y_WSrE6r7wk",
     "availableInventory": 5,
-    "rating": 
+    "rating": 4
   },
   {
     "id": 1007,
@@ -60,7 +60,7 @@ let products = [
     "price": 25,
     "image": "https://media-whichmedia.s3.ap-southeast-1.amazonaws.com/media/large/e/0/e0814526963e.jpeg",
     "availableInventory": 5,
-    "rating": 
+    "rating": 3
   },
   {
     "id": 1008,
@@ -69,7 +69,7 @@ let products = [
     "price": 25,
     "image": "https://i.pinimg.com/originals/90/81/e0/9081e0782766430555cffcb3129996a8.jpg",
     "availableInventory": 5,
-    "rating": 
+    "rating": 4
   },
   {
     "id": 1009,
@@ -78,7 +78,7 @@ let products = [
     "price": 25,
     "image": "https://lh3.googleusercontent.com/proxy/jBJ9G14qT2MMGLRsHg6XTvmtZsjoGFQLsMMxk6VgGXxBi5BdL9j7w88BOywT9MjU3iQL1ubcqwyrxC8aS1tbpS1b",
     "availableInventory": 5,
-    "rating": 
+    "rating": 5
   },
   {
     "id": 1010,
@@ -87,6 +87,6 @@ let products = [
     "price": 25,
     "image": "https://lh3.googleusercontent.com/proxy/3LgIoUOwUOnW1zRoZIzXVjKb5nNOwd06g0LhYOyJVtfFc93M5hp4kNrgiAY6fE6OyzaEEOHejdc4hXihz6Nayks2Qr-_9ZU",
     "availableInventory": 5,
-    "rating": 
+    "rating": 5
   }
 ]

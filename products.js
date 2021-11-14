75901899313ca2415dff35ad51e6d4556696ae96
ZacharyBaldock <93323069+ZacharyBaldock@users.noreply.github.com>
2021-11-14 17:09:21 +0400

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ let products = [
     "description": "For those who have trouble with maths",
     "price": 25,
     "image": "https://thumbs.dreamstime.com/b/math-mathematics-word-vintage-letterpress-wood-type-against-number-background-37860245.jpg",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -14,7 +14,7 @@ let products = [
     "description": "For those who have trouble with Chemistry",
     "price": 25,
     "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F98099-free-chemistry-vector&psig=AOvVaw3SmliSjAKhqzVkKqZzQiMA&ust=1636981141717000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjChZf0l_QCFQAAAAAdAAAAABAD",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -23,7 +23,7 @@ let products = [
     "description": "For those who have trouble with physics",
     "price": 25,
     "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fstock-photo%2Fphysics.html&psig=AOvVaw0NWcUjn_DDgclYEtOtOd4x&ust=1636981169596000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCDg6P0l_QCFQAAAAAdAAAAABAL",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -32,7 +32,7 @@ let products = [
     "description": "For those who need to catch up in Biology",
     "price": 25,
     "image": "https://moodle.gtni.edu.eg/pluginfile.php/2720/course/overviewfiles/biology-800x393.jpg",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -41,7 +41,7 @@ let products = [
     "description": "For those who need to catch up in English class",
     "price": 25,
     "image": "https://st2.depositphotos.com/3591429/5244/i/600/depositphotos_52449961-stock-photo-peoples-arms-holding-word-english.jpg",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   }
       {
@@ -50,7 +50,7 @@ let products = [
     "description": "So the subject isn't french to you",
     "price": 25,
     "image": "https://lh3.googleusercontent.com/proxy/9MQGTcKtKv3a6qqeaRdI1UFbfJs0CllWn5XSJALpW9-pMLyMd9Uf7ztapprrzSG877IT8kCgKZ0NNMFS6gSEH7Wa2TC4Y_WSrE6r7wk",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -59,7 +59,7 @@ let products = [
     "description": "such a beautiful languge",
     "price": 25,
     "image": "https://media-whichmedia.s3.ap-southeast-1.amazonaws.com/media/large/e/0/e0814526963e.jpeg",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -68,7 +68,7 @@ let products = [
     "description": "if your skills need tuning up",
     "price": 25,
     "image": "https://i.pinimg.com/originals/90/81/e0/9081e0782766430555cffcb3129996a8.jpg",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -77,7 +77,7 @@ let products = [
     "description": "So its not quite such a mystery",
     "price": 25,
     "image": "https://lh3.googleusercontent.com/proxy/jBJ9G14qT2MMGLRsHg6XTvmtZsjoGFQLsMMxk6VgGXxBi5BdL9j7w88BOywT9MjU3iQL1ubcqwyrxC8aS1tbpS1b",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   },
   {
@@ -86,7 +86,7 @@ let products = [
     "description": "Quite the employable Skill",
     "price": 25,
     "image": "https://lh3.googleusercontent.com/proxy/3LgIoUOwUOnW1zRoZIzXVjKb5nNOwd06g0LhYOyJVtfFc93M5hp4kNrgiAY6fE6OyzaEEOHejdc4hXihz6Nayks2Qr-_9ZU",
-    "availableInventory": ,
+    "availableInventory": 5,
     "rating": 
   }
 ]

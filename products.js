34f166158199a6d0cd937e6b1d11ceea06700b1e
ZacharyBaldock <93323069+ZacharyBaldock@users.noreply.github.com>
2021-11-14 16:41:59 +0400

    Update products.js

--- products.js
+++ products.js
@@ -44,4 +44,49 @@ let products = [
       "availableInventory": ,
       "rating": 
     }
+        {
+      "id": ,
+      "title": "",
+      "description": "",
+      "price": ,
+      "image": "",
+      "availableInventory": ,
+      "rating": 
+    },
+    {
+      "id": ,
+      "title": "",
+      "description": "",
+      "price": ,
+      "image": "",
+      "availableInventory": ,
+      "rating": 
+    },
+    {
+      "id": ,
+      "title": "",
+      "description": "",
+      "price": ,
+      "image": "",
+      "availableInventory": ,
+      "rating": 
+    },
+    {
+      "id": ,
+      "title": "",
+      "description": "",
+      "price": ,
+      "image": "",
+      "availableInventory": ,
+      "rating": 
+    },
+    {
+      "id": ,
+      "title": "",
+      "description": "",
+      "price": ,
+      "image": "",
+      "availableInventory": ,
+      "rating": 
+    }
   ]
